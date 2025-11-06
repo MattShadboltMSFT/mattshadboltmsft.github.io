742d98a265cbb0bf4b5fe5f11437c610eda4b0f1
Matt Shadbolt <39964111+MattShadboltMSFT@users.noreply.github.com>
2025-11-07 08:06:36 +1100

    fix again

--- src/pages/PasswordProtection.jsx
+++ src/pages/PasswordProtection.jsx
@@ -84,7 +84,7 @@ export default function PasswordProtection({ onAuthenticated }) {
                     }}
                   >
                     <img
-                      src="./images/jay.png"
+                      src="/images/jay.png"
                       alt="Jay"
                       style={{
                         width: '4rem',
